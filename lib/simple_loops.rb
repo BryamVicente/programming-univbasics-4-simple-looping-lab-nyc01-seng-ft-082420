16d7a19aaea379e617143f6dc86f7714d5e64f1b
Bryam Vicente <vicentebryam@gmail.com>
2020-08-10 22:46:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+def loop_message_five_times(string)
+  counter = 0
+  
+  puts ""
+
+end
+
+def loop_message_n_times(string,index)
+
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
